610b6ea66fcbb6f56a5b86bbee1a47571032cb5e
robharrison-ad <app.developer@mail.com>
2021-04-09 21:31:24 -0700

    additional fixes to heroku config

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/heartbeat'));
+app.use(express.static(__dirname + '/dist/charts'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/heartbeat/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/charts/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
